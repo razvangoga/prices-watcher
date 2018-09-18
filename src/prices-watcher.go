b3aa98307ddbca28d14ffdb7ea572209e5a027af
Razvan Goga <razvan.goga@gmail.com>
2018-09-18 17:19:02 +0200

    made parsing parallel with go routines and mesured each call

--- src/prices-watcher.go
+++ src/prices-watcher.go
@@ -2,14 +2,27 @@ package main
 
 import (
 	"log"
+	"sync"
+	"time"
 )
 
 func main() {
 
 	var urls = GetDataSource()
 
-	for _, page := range urls {
-		log.Printf("Fetching price for [%s] [%s]\n", page.product, page.site)
-		log.Printf("Found : %.2f\n", GetPrice(page))
+	var wg sync.WaitGroup
+	wg.Add(len(urls))
+
+	for _, pg := range urls {
+
+		go func(pg page) {
+			start := time.Now()
+			price := GetPrice(pg)
+			elapsed := time.Since(start)
+			log.Printf("[%s] [%s] - found : %.2f in %s\n", pg.product, pg.site, price, elapsed)
+			wg.Done()
+		}(pg)
 	}
+
+	wg.Wait()
 }
