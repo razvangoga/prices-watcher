ac5c34a17e488543f9e0e66aada1671c97d73896
Razvan Goga <razvan.goga@gmail.com>
2018-10-18 16:34:52 +0200

    modified packaging script

--- package.ps1
+++ package.ps1
@@ -4,13 +4,12 @@ Add-Type -assembly  System.IO.Compression.FileSystem
 $archive = [System.IO.Compression.ZipFile]::Open("$pwd/$jobName.zip","Create")
 
 $exe = "$pwd/$jobName.exe"
-$exeZipPath = "\Content\D_C\a\1\s\$jobName\obj\Debug\Package\PackageTmp\app_data\jobs\triggered\$jobName"
+$exeZipPath = "\app_data\jobs\triggered\$jobName"
 
 $settings = "$pwd/settings.json"
-$settingsZipPath = "\Content\D_C\a\1\s\$jobName\obj\Debug\Package\PackageTmp"
 
 $zexe = [System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($archive,$exe,"$exeZipPath\prices-watcher.exe","optimal")
-$zsettings = [System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($archive,$settings,"$settingsZipPath\settings.json","optimal")
+$zsettings = [System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($archive,$settings,"settings.json","optimal")
 
 $archive.Dispose()
 
