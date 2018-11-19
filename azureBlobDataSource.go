287bdd5380bab77172865547972e7296509e1710
Razvan Goga <razvan.goga@gmail.com>
2018-11-19 18:06:18 +0100

    updated azure blobstorage lib

--- azureBlobDataSource.go
+++ azureBlobDataSource.go
@@ -5,7 +5,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"github.com/Azure/azure-storage-blob-go/2016-05-31/azblob"
+	"github.com/Azure/azure-storage-blob-go/azblob"
 	"net/url"
 )
 
@@ -16,7 +16,12 @@ type AzureBlobDataSource struct {
 
 func (ads AzureBlobDataSource) get() pagesArray {
 
-	credential := azblob.NewSharedKeyCredential(ads.accountName, ads.accountKey)
+	credential, err := azblob.NewSharedKeyCredential(ads.accountName, ads.accountKey)
+
+	if err != nil {
+		panic(err)
+	}
+
 	p := azblob.NewPipeline(credential, azblob.PipelineOptions{})
 
 	URL, _ := url.Parse(fmt.Sprintf("https://%s.blob.core.windows.net/%s", ads.accountName, "priceswatcher"))
@@ -28,12 +33,14 @@ func (ads AzureBlobDataSource) get() pagesArray {
 
 	blobURL := containerURL.NewBlockBlobURL("dataSource.json")
 
-	stream := azblob.NewDownloadStream(ctx, blobURL.GetBlob, azblob.DownloadStreamOptions{})
+	downloadResponse, err := blobURL.Download(ctx, 0, azblob.CountToEnd, azblob.BlobAccessConditions{}, false)
+	stream := downloadResponse.Body(azblob.RetryReaderOptions{MaxRetryRequests: 20})
+
 	downloadedData := &bytes.Buffer{}
 	_, _ = downloadedData.ReadFrom(stream)
 
 	pages := pagesArray{}
-	err := json.Unmarshal(downloadedData.Bytes(), &pages)
+	err = json.Unmarshal(downloadedData.Bytes(), &pages)
 
 	if err != nil {
 		panic(err)
